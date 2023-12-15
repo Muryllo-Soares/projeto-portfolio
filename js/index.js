d26698aaa7d99f67a70c32f2e37e76a43bdc4705
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-12-15 17:22:46 -0300

    criação dos mediaquery e criação do menu hamburgue utilizando logica de javascript.

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const buttonMenu = document.querySelector('.button_menu')
+
+const menuMobile = document.querySelector('.nav_mobile')
+let isMenuOpen = false
+
+buttonMenu.addEventListener('click', function(){
+    isMenuOpen ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible')
+
+    isMenuOpen ? buttonMenu.children[0].setAttribute('src', '../image/menu-hamburgue.png') : buttonMenu.children[0].setAttribute('src', '../image/meu-close.png')
+
+    isMenuOpen = !isMenuOpen
+    console.log(isMenuOpen)
+    
+
+})
