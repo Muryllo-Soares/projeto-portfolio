3f289e7330d2f6573a2bcea65946a8c95247924a
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-12-16 16:05:10 -0300

    Ajuste na logica do javascript e personalizando a aba de confirmacao

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let isMenuOpen = false
 buttonMenu.addEventListener('click', function(){
     isMenuOpen ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible')
 
-    isMenuOpen ? buttonMenu.children[0].setAttribute('src', '../image/menu-hamburgue.png') : buttonMenu.children[0].setAttribute('src', '../image/meu-close.png')
+    isMenuOpen ? buttonMenu.children[0].setAttribute('src', '../image/menu-hamburgue.png') : buttonMenu.children[0].setAttribute('src', '../image/menu-close.png')
 
     isMenuOpen = !isMenuOpen
     console.log(isMenuOpen)
