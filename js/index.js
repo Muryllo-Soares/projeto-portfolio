70f46f20795583eb1e090e62dc4079789451a548
Muryllo Soares <111825569+Muryllo-Soares@users.noreply.github.com>
2023-12-16 16:14:13 -0300

    ajuste botão X menu mobile

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ const menuMobile = document.querySelector('.nav_mobile')
 let isMenuOpen = false
 
 buttonMenu.addEventListener('click', function(){
-    isMenuOpen ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible')
+    isMenuOpen ? menuMobile.classList.add('invisible') : menuMobile.classList.remove('invisible');
 
-    isMenuOpen ? buttonMenu.children[0].setAttribute('src', '../image/menu-hamburgue.png') : buttonMenu.children[0].setAttribute('src', '../image/menu-close.png')
+    isMenuOpen ? buttonMenu.children[0].setAttribute('src', '../image/menu-hamburgue.png') : buttonMenu.children[0].setAttribute('src', '../image/meu-close.png')
 
     isMenuOpen = !isMenuOpen
     console.log(isMenuOpen)
